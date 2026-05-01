20b4d2362ffe6ca48569ef3d29888bf1216f3077
aasthapanwar2 <148461334+aasthapanwar2@users.noreply.github.com>
2026-05-01 17:44:01 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,354 +1,81 @@
-import Notice from "../models/notification.js";
-import Task from "../models/task.js";
-import User from "../models/user.js";
+import React, { useState } from "react";
+import { FaList } from "react-icons/fa";
+import { MdGridView } from "react-icons/md";
+import { useParams } from "react-router-dom";
+import Loading from "../components/Loader";
+import Title from "../components/Title";
+import Button from "../components/Button";
+import { IoMdAdd } from "react-icons/io";
+import Tabs from "../components/Tabs";
+import TaskTitle from "../components/TaskTitle";
+import BoardView from "../components/BoardView";
+import { tasks } from "../assets/data";
+import Table from "../components/task/Table";
+import AddTask from "../components/task/AddTask";
 
-export const createTask = async (req, res) => {
-  try {
-    const { userId } = req.user;
+const TABS = [
+  { title: "Board View", icon: <MdGridView /> },
+  { title: "List View", icon: <FaList /> },
+];
 
-    const { title, team, stage, date, priority, assets } = req.body;
-
-    let text = "New task has been assigned to you";
-    if (team?.length > 1) {
-      text = text + ` and ${team?.length - 1} others.`;
-    }
-
-    text =
-      text +
-      ` The task priority is set a ${priority} priority, so check and act accordingly. The task date is ${new Date(
-        date
-      ).toDateString()}. Thank you!!!`;
-
-    const activity = {
-      type: "assigned",
-      activity: text,
-      by: userId,
-    };
-
-    const task = await Task.create({
-      title,
-      team,
-      stage: stage.toLowerCase(),
-      date,
-      priority: priority.toLowerCase(),
-      assets,
-      activities: activity,
-    });
-
-    await Notice.create({
-      team,
-      text,
-      task: task._id,
-    });
-
-    res
-      .status(200)
-      .json({ status: true, task, message: "Task created successfully." });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
+const TASK_TYPE = {
+  todo: "bg-blue-600",
+  "in progress": "bg-yellow-600",
+  completed: "bg-green-600",
 };
 
-export const duplicateTask = async (req, res) => {
-  try {
-    const { id } = req.params;
+const Tasks = () => {
+  const params = useParams();
 
-    const task = await Task.findById(id);
+  const [selected, setSelected] = useState(0);
+  const [open, setOpen] = useState(false);
+  const [loading, setLoading] = useState(false);
 
-    const newTask = await Task.create({
-      ...task,
-      title: task.title + " - Duplicate",
-    });
+  const status = params?.status || "";
 
-    newTask.team = task.team;
-    newTask.subTasks = task.subTasks;
-    newTask.assets = task.assets;
-    newTask.priority = task.priority;
-    newTask.stage = task.stage;
+  return loading ? (
+    <div className='py-10'>
+      <Loading />
+    </div>
+  ) : (
+    <div className='w-full'>
+      <div className='flex items-center justify-between mb-4'>
+        <Title title={status ? `${status} Tasks` : "Tasks"} />
 
-    await newTask.save();
+        {!status && (
+          <Button
+            onClick={() => setOpen(true)}
+            label='Create Task'
+            icon={<IoMdAdd className='text-lg' />}
+            className='flex flex-row-reverse gap-1 items-center bg-blue-600 text-white rounded-md py-2 2xl:py-2.5'
+          />
+        )}
+      </div>
 
-    //alert users of the task
-    let text = "New task has been assigned to you";
-    if (task.team.length > 1) {
-      text = text + ` and ${task.team.length - 1} others.`;
-    }
+      <Tabs tabs={TABS} setSelected={setSelected}>
+        {!status && (
+          <div className='w-full flex justify-between gap-4 md:gap-x-12 py-4'>
+            <TaskTitle label='To Do' className={TASK_TYPE.todo} />
+            <TaskTitle
+              label='In Progress'
+              className={TASK_TYPE["in progress"]}
+            />
+            <TaskTitle label='completed' className={TASK_TYPE.completed} />
+          </div>
+        )}
 
-    text =
-      text +
-      ` The task priority is set a ${
-        task.priority
-      } priority, so check and act accordingly. The task date is ${task.date.toDateString()}. Thank you!!!`;
+        {selected !== 1 ? (
+          <BoardView tasks={tasks} />
+        ) : (
+          <div className='w-full'>
+            <Table tasks={tasks} />
+          </div>
+        )}
+      </Tabs>
 
-    await Notice.create({
-      team: task.team,
-      text,
-      task: newTask._id,
-    });
-
-    res
-      .status(200)
-      .json({ status: true, message: "Task duplicated successfully." });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
+      <AddTask open={open} setOpen={setOpen} />
+    </div>
+  );
 };
 
-export const postTaskActivity = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { userId } = req.user;
-    const { type, activity } = req.body;
-
-    const task = await Task.findById(id);
-
-    const data = {
-      type,
-      activity,
-      by: userId,
-    };
-
-    task.activities.push(data);
-
-    await task.save();
-
-    res
-      .status(200)
-      .json({ status: true, message: "Activity posted successfully." });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const dashboardStatistics = async (req, res) => {
-  try {
-    const { userId, isAdmin } = req.user;
-
-    const allTasks = isAdmin
-      ? await Task.find({
-          isTrashed: false,
-        })
-          .populate({
-            path: "team",
-            select: "name role title email",
-          })
-          .sort({ _id: -1 })
-      : await Task.find({
-          isTrashed: false,
-          team: { $all: [userId] },
-        })
-          .populate({
-            path: "team",
-            select: "name role title email",
-          })
-          .sort({ _id: -1 });
-
-    const users = await User.find({ isActive: true })
-      .select("name title role isAdmin createdAt")
-      .limit(10)
-      .sort({ _id: -1 });
-
-    //   group task by stage and calculate counts
-    const groupTaskks = allTasks.reduce((result, task) => {
-      const stage = task.stage;
-
-      if (!result[stage]) {
-        result[stage] = 1;
-      } else {
-        result[stage] += 1;
-      }
-
-      return result;
-    }, {});
-
-    // Group tasks by priority
-    const groupData = Object.entries(
-      allTasks.reduce((result, task) => {
-        const { priority } = task;
-
-        result[priority] = (result[priority] || 0) + 1;
-        return result;
-      }, {})
-    ).map(([name, total]) => ({ name, total }));
-
-    // calculate total tasks
-    const totalTasks = allTasks?.length;
-    const last10Task = allTasks?.slice(0, 10);
-
-    const summary = {
-      totalTasks,
-      last10Task,
-      users: isAdmin ? users : [],
-      tasks: groupTaskks,
-      graphData: groupData,
-    };
-
-    res.status(200).json({
-      status: true,
-      message: "Successfully",
-      ...summary,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const getTasks = async (req, res) => {
-  try {
-    const { stage, isTrashed } = req.query;
-
-    let query = { isTrashed: isTrashed ? true : false };
-
-    if (stage) {
-      query.stage = stage;
-    }
-
-    let queryResult = Task.find(query)
-      .populate({
-        path: "team",
-        select: "name title email",
-      })
-      .sort({ _id: -1 });
-
-    const tasks = await queryResult;
-
-    res.status(200).json({
-      status: true,
-      tasks,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const getTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const task = await Task.findById(id)
-      .populate({
-        path: "team",
-        select: "name title role email",
-      })
-      .populate({
-        path: "activities.by",
-        select: "name",
-      });
-
-    res.status(200).json({
-      status: true,
-      task,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const createSubTask = async (req, res) => {
-  try {
-    const { title, tag, date } = req.body;
-
-    const { id } = req.params;
-
-    const newSubTask = {
-      title,
-      date,
-      tag,
-    };
-
-    const task = await Task.findById(id);
-
-    task.subTasks.push(newSubTask);
-
-    await task.save();
-
-    res
-      .status(200)
-      .json({ status: true, message: "SubTask added successfully." });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const updateTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { title, date, team, stage, priority, assets } = req.body;
-
-    const task = await Task.findById(id);
-
-    task.title = title;
-    task.date = date;
-    task.priority = priority.toLowerCase();
-    task.assets = assets;
-    task.stage = stage.toLowerCase();
-    task.team = team;
-
-    await task.save();
-
-    res
-      .status(200)
-      .json({ status: true, message: "Task duplicated successfully." });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const trashTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-
-    const task = await Task.findById(id);
-
-    task.isTrashed = true;
-
-    await task.save();
-
-    res.status(200).json({
-      status: true,
-      message: `Task trashed successfully.`,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
-
-export const deleteRestoreTask = async (req, res) => {
-  try {
-    const { id } = req.params;
-    const { actionType } = req.query;
-
-    if (actionType === "delete") {
-      await Task.findByIdAndDelete(id);
-    } else if (actionType === "deleteAll") {
-      await Task.deleteMany({ isTrashed: true });
-    } else if (actionType === "restore") {
-      const resp = await Task.findById(id);
-
-      resp.isTrashed = false;
-      resp.save();
-    } else if (actionType === "restoreAll") {
-      await Task.updateMany(
-        { isTrashed: true },
-        { $set: { isTrashed: false } }
-      );
-    }
-
-    res.status(200).json({
-      status: true,
-      message: `Operation performed successfully.`,
-    });
-  } catch (error) {
-    console.log(error);
-    return res.status(400).json({ status: false, message: error.message });
-  }
-};
+export default Tasks;
